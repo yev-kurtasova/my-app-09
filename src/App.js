1fabd3f9fb316dbe782a34242f626e06c0aa72fe
yev-kurtasova <eugenia.kurtasova@gmail.com>
2024-01-12 16:18:47 +0200

    create weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState({});
+  const [city, setCity] = useState('');
+
+  const key = 'db28ed587957b83b6f4cc48e04ba39a6';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`
+
+  const onSearchWeather = (e) => {
+    if (e.key === 'Enter') {
+      fetch(url)
+        .then(response => response.json())
+        .then(res => {
+          console.log(res);
+          setData(res);
+        })
+      setCity('');
+
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="inp-field">
+        <input type="text"
+          placeholder='Enter Location'
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          onKeyDown={onSearchWeather}
+        />
+      </div>
+      <div className="container">
+        <div className="header">
+          <div className="city">
+            <p>{data.name}</p>
+          </div>
+
+          <div className="temp">
+            {data.main && <h1>{data.main.temp.toFixed()}°C</h1>}
+          </div>
+
+          <div className="desc">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
